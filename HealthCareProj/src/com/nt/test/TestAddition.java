c184ad9463a4af5d8139ca83f45b16b8a3e2ba58
HP <HP@DESKTOP-7JAJ0QJ>
2024-03-14 16:28:34 -0400

    Dev checks addition functionality

--- HealthCareProj/src/com/nt/test/TestAddition.java
+++ HealthCareProj/src/com/nt/test/TestAddition.java
@@ -5,6 +5,7 @@ import com.nt.service.AddOperation;
 public class TestAddition {
 public static void main(String[] args) {
 	AddOperation add=new AddOperation();
-	System.out.println("Add value :"+add.add(2, 3));
+	System.out.println("Add value 2 and 3 :"+add.add(2, 3));
+System.out.println("Addition value 10 and 20:"+add.add(10, 20));;
 }
 }
