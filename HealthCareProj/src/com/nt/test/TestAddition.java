3ca8831e57bd36eb85510782b9b505ea79388acd
HP <HP@DESKTOP-7JAJ0QJ>
2024-03-14 16:43:57 -0400

    TL perform addition

--- HealthCareProj/src/com/nt/test/TestAddition.java
+++ HealthCareProj/src/com/nt/test/TestAddition.java
@@ -7,6 +7,7 @@ import com.nt.service.AddOperation;
 public class TestAddition {
 	public static void main(String[] args[]) {
 		AddOperation addition=new AddOperation();
+		System.out.println("Addition value of 45 and 45:"+addition.add(45, 45));
 	
 	}
 }
