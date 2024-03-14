91fbf6bc7c4b2113feb699b612c6aeedf2c061de
HP <HP@DESKTOP-7JAJ0QJ>
2024-03-14 16:41:00 -0400

    tl added addition method

---
+++ HealthCareProj/src/com/nt/test/TestAddition.java
@@ -0,0 +1,12 @@
+package com.nt.test;
+
+import com.nt.service.AddOperation;
+
+import com.nt.service.AddOperation;
+
+public class TestAddition {
+	public static void main(String[] args[]) {
+		AddOperation addition=new AddOperation();
+	
+	}
+}
