45496c42567ee35c925aeeb2914e148c3db628f9
HP <HP@DESKTOP-7JAJ0QJ>
2024-03-14 16:46:09 -0400

    dev perform addition

--- HealthCareProj/src/com/nt/test/TestAddition.java
+++ HealthCareProj/src/com/nt/test/TestAddition.java
@@ -7,6 +7,7 @@ import com.nt.service.AddOperation;
 public class TestAddition {
 	public static void main(String[] args[]) {
 		AddOperation addition=new AddOperation();
-	
+		System.out.println("Addition value 35 and 35:"+addition.add(35, 35));
+
 	}
 }
